c2dbf17035476c153dafa5b93364641925b3cee4
Mmustafa-772002 <116543187+Mmustafa-772002@users.noreply.github.com>
2023-10-10 12:39:20 +0530

    Update variables.c

--- Programming Topics/Chapter 1/Variables/variables.c
+++ Programming Topics/Chapter 1/Variables/variables.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 int main() {
+    // Declaration of variables
     int a = 20;
     float b = 3.2;
     char c = 'a';
     char d[] = "this is a string ";
     double e = 3.1415;
 
+    // Printing sizes of data types
     printf("Size of int: %zu bytes\n", sizeof(a));
     printf("Size of float: %zu bytes\n", sizeof(b));
     printf("Size of char: %zu bytes\n", sizeof(c));
     printf("Size of string: %zu bytes\n", sizeof(d));
     printf("Size of double: %zu bytes\n", sizeof(e));
 
+    // Printing values of variables
     printf("The value of int is: %d\n", a);
     printf("The value of float is: %f\n", b);
     printf("The value of char is: %c\n", c);
